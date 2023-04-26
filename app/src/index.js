3b72dc6696ccbcff611f1bd8d8b64b71e4b107ef
jduron24 <jduron24@iastate.edu>
2023-04-25 19:29:35 -0500

    json file

--- app/src/index.js
+++ app/src/index.js
@@ -70,3 +70,10 @@ root.render(
 //step 1: git add --all
 //step 2: git commit -m "Ex" (add a message in the quotes)
 //step 3: git push --set-upstream origin main (use this to push things to github)
+
+/* 
+To pull 
+Step 1: git pull <Username> main (type in the collaborators username)
+this will pull all of the collaborators code to your local computer
+
+*/
