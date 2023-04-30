cbbe6358fad9b42bd9eea7dfa8a356f1b7d75f8b
jduron24 <jduron24@iastate.edu>
2023-04-29 21:44:36 -0500

    update 4/29/23

--- backend/index.js
+++ backend/index.js
@@ -9,14 +9,14 @@ app.use(cors());
 
 
 app.use(express.static("public"));
-app.use("/images",express.static("images"))
+app.use("/images", express.static("images"))
 
 mongoose.connect("mongodb://127.0.0.1:27017/reactdata",
-{
-dbName: "reactdata",
-useNewUrlParser: true,
-useUnifiedTopology: true,
-}
+    {
+        dbName: "reactdata",
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    }
 );
 const port = process.env.PORT || 3000;
 const host = "localhost";
@@ -26,12 +26,42 @@ app.get("/", async (req, resp) => {
     const allProducts = await Product.find(query);
     console.log(allProducts);
     resp.send(allProducts);
-    });
-    
+});
 
+app.post("/insert", async (req, res) => {
+    console.log(req.body);
+    const p_id = req.body._id;
+    const ptitle = req.body.title;
+    const pprice = req.body.price;
+    const pdescription = req.body.description;
+    const pcategory = req.body.category;
+    const pimage = req.body.image;
+    const prate = req.body.rating.rate;
+    const pcount = req.body.rating.count;
+
+
+const formData = new Product({
+    _id: p_id,
+    title: ptitle,
+    price: pprice,
+    description: pdescription,
+    category: pcategory,
+    image: pimage,
+    rating: { rate: prate, count: pcount },
+    });
+
+    try {
+        // await formData.save();
+        await Product.create(formData);
+        const messageResponse = { message: `Product ${p_id} added correctly` };
+        res.send(JSON.stringify(messageResponse));
+        } catch (err) {
+        console.log("Error while adding a new product:" + err);
+        };
+});
 
 app.listen(port, () => {
-console.log(`App listening at http://%s:%s`, host, port);
+    console.log(`App listening at http://%s:%s`, host, port);
 });
 
 
@@ -41,4 +71,19 @@ app.get("/:id", async (req, resp) => {
     const oneProduct = await Product.findOne(query);
     console.log(oneProduct);
     resp.send(oneProduct);
+});
+
+
+app.delete("/delete", async (req, res) => {
+    console.log("Delete :", req.body);
+    try {
+    const query = { _id: req.body._id };
+    await Product.deleteOne(query);
+    const messageResponse = {
+    message: `Product ${req.body._id} deleted correctly`,
+    };
+    res.send(JSON.stringify(messageResponse));
+    } catch (err) {
+    console.log("Error while deleting :" + p_id + " " + err);
+    }
     });
